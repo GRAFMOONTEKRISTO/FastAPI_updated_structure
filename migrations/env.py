9123fa7f4697fb92019a46315f54edc7c8c0e2a5
GRAFMOONTEKRISTO <109302818+GRAFMOONTEKRISTO@users.noreply.github.com>
2023-04-26 21:51:42 +0300

    added auth

--- migrations/env.py
+++ migrations/env.py
@@ -4,14 +4,14 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from config import DB_PORT, DB_USER, DB_HOST, DB_NAME, DB_PASS
+
+from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
 from models.models import metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-# прописываем конфиг, добавляя элементы, которые далее пойдут в .env и в alembic.ini для защиты данных
-# урок 4
+
 section = config.config_ini_section
 config.set_section_option(section, "DB_HOST", DB_HOST)
 config.set_section_option(section, "DB_PORT", DB_PORT)
@@ -28,8 +28,6 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-
-# нужно из папки моделей импортировать metadata
 target_metadata = metadata
 
 
@@ -41,15 +39,12 @@ target_metadata = metadata
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
-
     This configures the context with just a URL
     and not an Engine, though an Engine is acceptable
     here as well.  By skipping the Engine creation
     we don't even need a DBAPI to be available.
-
     Calls to context.execute() here emit the given string to the
     script output.
-
     """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
@@ -65,13 +60,11 @@ def run_migrations_offline() -> None:
 
 def run_migrations_online() -> None:
     """Run migrations in 'online' mode.
-
     In this scenario we need to create an Engine
     and associate a connection with the context.
-
     """
     connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
+        config.get_section(config.config_ini_section),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
     )
